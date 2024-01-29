650c63dbda7f936a04cbe8d6cbba4526daf8b387
Czeglédi Levente <celevicte@gmail.com>
2024-01-29 23:44:46 +0100

    finalize sheet design and make shatter ui into components

--- app/lib/components/AddPlayerSheet.tsx
+++ app/lib/components/AddPlayerSheet.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import {
   Sheet,
@@ -12,40 +21,123 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { useGameActions } from "@/store/GameProvider";
-import { FC, useRef } from "react";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { useMedia, useToggle } from "react-use";
 
-const AddPlayerSheet: FC<{ side: "top" | "bottom" }> = ({ side }) => {
-  const newPlayerInputRef = useRef<HTMLInputElement>(null);
+const AddPlayerSheet: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    formState: { errors },
+  } = useForm<{ nickname: string }>({
+    resolver: (data) => {
+      if (!data.nickname?.trim()) {
+        return {
+          values: {},
+          errors: {
+            nickname: {
+              type: "required",
+              message: "Player name is required",
+            },
+          },
+        };
+      }
+      if (data.nickname.length > 16) {
+        return {
+          values: {},
+          errors: {
+            nickname: {
+              type: "maxLength",
+              message: "Player name is too long",
+            },
+          },
+        };
+      }
+      return {
+        values: data,
+        errors: {},
+      };
+    },
+  });
   const addPlayer = useGameActions().addPlayer;
 
+  const [open, toggleOpen] = useToggle(false);
+  const isDesktop = useMedia("(min-width: 768px)", true);
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={toggleOpen}>
+        <DialogTrigger asChild>
+          <Button>Add player</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px] flex flex-col justify-start items-start px-6 pb-8 gap-4">
+          <DialogHeader>
+            <DialogTitle>Add new Player</DialogTitle>
+          </DialogHeader>
+          <Input
+            placeholder="Player name"
+            className="w-full"
+            {...register("nickname")}
+            autoFocus
+          />
+          <DialogFooter className="flex items-start justify-between w-full">
+            {!!errors.nickname ? (
+              <span className="text-red-600 text-base leading-none">
+                {errors.nickname.message}
+              </span>
+            ) : (
+              <div />
+            )}
+            <DialogClose asChild>
+              <Button
+                className="self-end"
+                onClick={handleSubmit((data) => {
+                  addPlayer(data.nickname);
+                  toggleOpen(false);
+                  resetField("nickname");
+                })}
+              >
+                Add
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={toggleOpen}>
       <SheetTrigger asChild>
-        <Button>Add player sheet |{side}|</Button>
+        <Button onClick={toggleOpen}>Add player</Button>
       </SheetTrigger>
       <SheetContent
-        side={side}
-        className="flex flex-col gap-5 border-slate-600 rounded-t-md shadow-md"
+        side="top"
+        className="flex flex-col gap-5  rounded-b-md shadow-md"
       >
         <SheetHeader>
           <SheetTitle>Add new player</SheetTitle>
         </SheetHeader>
-        <Input
-          placeholder="Player name"
-          ref={newPlayerInputRef}
-          className="text-base"
-        />
+        <Input placeholder="Player name" {...register("nickname")} />
 
-        <SheetFooter>
+        <SheetFooter className="flex items-start flex-row justify-between w-full">
+          {!!errors.nickname ? (
+            <span className="text-red-600 text-base leading-none">
+              {errors.nickname.message}
+            </span>
+          ) : (
+            <div />
+          )}
           <SheetClose asChild>
             <Button
               type="button"
-              className="self-end"
-              onClick={() =>
-                // TODO: add validation
-                newPlayerInputRef.current &&
-                addPlayer(newPlayerInputRef.current.value)
-              }
+              onClick={handleSubmit((data) => {
+                addPlayer(data.nickname);
+                resetField("nickname");
+                toggleOpen(false);
+              })}
             >
               Add
             </Button>
---
+++ app/lib/components/PlayersList.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { selectPointerAbsorbAtom } from "@/app/lib/atoms";
+import AddPlayerSheet from "@/app/lib/components/AddPlayerSheet";
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import {
+  useCanStartGame,
+  useGameActions,
+  usePlayers,
+} from "@/store/GameProvider";
+import { useAtomValue, useStore } from "jotai";
+import { FC, useEffect, useRef } from "react";
+import { useDebounce, useToggle } from "react-use";
+
+const PlayersList: FC = () => {
+  const store = useStore();
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const players = usePlayers();
+  const actions = useGameActions();
+  const canStartGame = useCanStartGame();
+  const [pointerEventsDebounced, togglePointerEventsDebounced] =
+    useToggle(false);
+  const isSelectOpen = useAtomValue(selectPointerAbsorbAtom, { store });
+  useDebounce(
+    () => {
+      togglePointerEventsDebounced(isSelectOpen);
+    },
+    50,
+    [isSelectOpen]
+  );
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [players.length]);
+
+  return (
+    <div
+      className={cn(
+        "h-full min-h-0 flex flex-col justify-end gap-2",
+        pointerEventsDebounced ? "pointer-events-none" : "pointer-events-auto"
+      )}
+    >
+      <h2 className="font-semibold text-xl">Players</h2>
+      {!players.length ? (
+        <div className="flex flex-grow items-center justify-center h-full">
+          No players added yet
+        </div>
+      ) : (
+        <ScrollArea className="flex flex-col w-full gap-4 h-full overflow-y-aut0">
+          {players.map((player) => (
+            <div key={player.id} className="flex flex-col gap-1.5 w-full">
+              <div className="gap-1 w-full items-center justify-between flex pt-1.5">
+                <h3 className="font-semibold">{player.name}</h3>
+                <Button
+                  variant="ghost"
+                  className="text-red-500 text-xs"
+                  onClick={() => actions.removePlayer(player.id)}
+                >
+                  Remove
+                </Button>
+              </div>
+              <Separator />
+            </div>
+          ))}
+          <ScrollBar />
+          <div ref={scrollRef} />
+        </ScrollArea>
+      )}
+      <AddPlayerSheet />
+      <div className="flex items-center justify-between w-full pt-2">
+        <Button variant="destructive" onClick={actions.reset}>
+          Reset
+        </Button>
+        <Button disabled={!canStartGame} onClick={actions.startGame}>
+          Start
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PlayersList;
