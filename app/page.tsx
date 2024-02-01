1a8ec48d3002cd52b00f88f65b1781a529082400
LeventeCzegledi-Hiya <levente.czegledi@hiya.com>
2024-02-01 23:14:35 +0100

    fix game start issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@
 import { useGameStore } from "@/store/GameProvider";
 import { FC, useCallback } from "react";
 
+import Game from "@/app/lib/components/Game";
 import GameStarterForm from "@/app/lib/components/GameStarterForm";
 
-const Game: FC = () => {
+const Home: FC = () => {
   const hasGameStarted = useGameStore()(
     useCallback((store) => store.hasGameStarted, [])
   );
   return (
-    <main className="flex h-[100svh] flex-col mx-auto items-center gap-10 container py-3 px-5">
+    <main className="flex h-[100svh] flex-col mx-auto items-center gap-10 container p-5">
       {hasGameStarted ? <Game /> : <GameStarterForm />}
     </main>
   );
 };
-export default Game;
+export default Home;
